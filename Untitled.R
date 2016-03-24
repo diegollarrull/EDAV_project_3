9b10e50d8559a5ad80d6ba2c4f376fd9e639bb1f
masta-g3 <manolorueda@gmail.com>
2016-03-24 13:46:47 -0400

    code for plots

--- Untitled.R
+++ Untitled.R
@@ -9,12 +9,15 @@ library(animation)
 
 yelp <- read.csv("yelp api/yelp.csv")
 yelp_rating <- read.csv("yelp api/yelp_rating.csv")
+yelp_distance <- read.csv("yelp api/yelp_distance.csv")
 
 dat <- read.csv("clean-data/Traffic_Violations_alcohol.csv", stringsAsFactors = F)
 ## Latitude and longitude coordinates are (sometimes) incorrect...
 dat$New.Lat <- pmax(dat$Latitude, dat$Longitude)
 dat$New.Lon <- pmin(dat$Latitude, dat$Longitude)
 dat$Date.Of.Stop <- as.Date(dat$Date.Of.Stop, format = "%m/%d/%Y")
+
+write.csv(dat, "fixed_violations.csv")
 
 ## Violations by minute.
 dat %>%
@@ -55,22 +58,86 @@ test.dat <- dat[sample(nrow(dat)),] %>%
   filter(!is.na(Latitude)) %>%
   head(500)
 
-## Contour Plot.
+## General map.
 map <- get_map(location = c(lon = -77.129074, lat = 39.10359), zoom =11, maptype = "roadmap")
 
 alcohol.map <- ggmap(map,  extent="panel") %+%
   unique(dat[!is.na(dat$New.Lat),c("New.Lat", "New.Lon")]) +
   aes(x = New.Lon, y = New.Lat) +
-  stat_density2d(aes(fill = ..level.., alpha = ..level..), geom = "polygon") +
+  stat_density2d(aes(fill = ..level..), alpha = 0.1, geom = "polygon") +
   scale_fill_gradient(low = "orange", high = "red") +
-  scale_alpha(range = c(0.1, 0.3)) +
+  #scale_alpha(range = c(0.1, 0.3)) +
   labs(x = "Longitude", y = "Latitude", title = "Montgomery County, Maryland") +
   theme(legend.position = "none") +
   coord_map()
 
 alcohol.map +
   geom_point(aes(x = New.Lon, y = New.Lat), fill="red", shape=21, alpha=0.3, size = 1) +
-  geom_point(data = yelp, aes(x = longitude, y = latitude), fill="blue", alpha = 0.7, shape=21, size = 3)
+  geom_point(data = unique(yelp_rating[yelp_rating$rating >= 3.5,]),
+             aes(x = longitude, y = latitude, alpha = 6**rating, size = 2**rating),
+             fill="black", shape=23) + 
+  scale_alpha(range = c(0.4, 0.8))
+
+
+## Zoom 1.
+map <- get_map(location = c(lon = -77.26, lat = 39.18), zoom =13, maptype = "roadmap")
+
+alcohol.map <- ggmap(map,  extent="panel") %+%
+  unique(dat[!is.na(dat$New.Lat),c("New.Lat", "New.Lon")]) +
+  aes(x = New.Lon, y = New.Lat) +
+  stat_density2d(aes(fill = ..level..), alpha = 0.2, geom = "polygon") +
+  scale_fill_gradient(low = "orange", high = "red") +
+  #scale_alpha(range = c(0.1, 0.3)) +
+  labs(x = "Longitude", y = "Latitude", title = "Germantown, Maryland") +
+  theme(legend.position = "none") +
+  coord_map()
+
+alcohol.map +
+  geom_point(aes(x = New.Lon, y = New.Lat), fill="red", shape=21, alpha=0.5, size = 1) +
+  geom_point(data = unique(yelp_rating[yelp_rating$rating >= 0,]),
+             aes(x = longitude, y = latitude, size = rating),
+             fill="black", shape=23, alpha = 0.8) + 
+  scale_alpha(range = c(0.4, 0.8))
+
+## Zoom 2.
+map <- get_map(location = c(lon = -77.15, lat = 39.09), zoom =14, maptype = "roadmap")
+
+alcohol.map <- ggmap(map,  extent="panel") %+%
+  unique(dat[!is.na(dat$New.Lat),c("New.Lat", "New.Lon")]) +
+  aes(x = New.Lon, y = New.Lat) +
+  stat_density2d(aes(fill = ..level..), alpha = 0.2, geom = "polygon") +
+  scale_fill_gradient(low = "orange", high = "red") +
+  #scale_alpha(range = c(0.1, 0.3)) +
+  labs(x = "Longitude", y = "Latitude", title = "Rockville, Maryland") +
+  theme(legend.position = "none") +
+  coord_map()
+
+alcohol.map +
+  geom_point(aes(x = New.Lon, y = New.Lat), fill="red", shape=21, alpha=0.5, size = 1) +
+  geom_point(data = unique(yelp_rating[yelp_rating$rating >= 0,]),
+             aes(x = longitude, y = latitude, size = rating),
+             fill="black", shape=23, alpha = 0.8) + 
+  scale_alpha(range = c(0.4, 0.8))
+
+## Zoom 3.
+map <- get_map(location = c(lon = -77.1, lat = 39.06), zoom =13, maptype = "roadmap")
+
+alcohol.map <- ggmap(map,  extent="panel") %+%
+  unique(dat[!is.na(dat$New.Lat),c("New.Lat", "New.Lon")]) +
+  aes(x = New.Lon, y = New.Lat) +
+  stat_density2d(aes(fill = ..level..), alpha = 0.2, geom = "polygon") +
+  scale_fill_gradient(low = "orange", high = "red") +
+  #scale_alpha(range = c(0.1, 0.3)) +
+  labs(x = "Longitude", y = "Latitude", title = "North Bethesda, Maryland") +
+  theme(legend.position = "none") +
+  coord_map()
+
+alcohol.map +
+  geom_point(aes(x = New.Lon, y = New.Lat), fill="red", shape=21, alpha=0.5, size = 1) +
+  geom_point(data = unique(yelp_rating[yelp_rating$rating >= 0,]),
+             aes(x = longitude, y = latitude, size = rating),
+             fill="black", shape=23, alpha = 0.8) + 
+  scale_alpha(range = c(0.4, 0.8))
 
 ##################
 ## Single Day M ##
@@ -100,11 +167,22 @@ frame1$Time <- c(t1, t2)
 
 ## Final time data frame.
 frame2 <- frame1 %>%
-  left_join(unique(bad.dat[c("Time", "New.Lat", "New.Lon", "Personal.Injury")]), by = "Time")
+  left_join(unique(bad.dat[c("Time", "New.Lat", "New.Lon", "Personal.Injury")]), by = "Time") %>%
+  mutate(Impact = 0)
 
 frame2$Personal.Injury <- ifelse(is.na(frame2$Personal.Injury), 0, 1)
 frame2[is.na(frame2$New.Lat),c(2,3)] <- 0
 
+i <- which(frame2$New.Lat != 0)
+
+## Add a "size" component.
+frame2[i,]$Impact <- 5
+frame2[i+1,c("New.Lat", "New.Lon")] <- frame2[i,c("New.Lat", "New.Lon")]
+frame2[i+1,]$Impact <- 3
+frame2[i+2,c("New.Lat", "New.Lon")] <- frame2[i,c("New.Lat", "New.Lon")]
+frame2[i+2,]$Impact <- 1
+
+## Make final GIF!!!
 map <- get_map(location = c(lon = -77.129074, lat = 39.10359), zoom =10 , maptype = "roadmap")
 
 saveGIF({
@@ -112,11 +190,11 @@ saveGIF({
     
     night.map <- ggmap(map, extent="panel") %+%
       frame2[i,] +
-      aes(x = New.Lon, y = New.Lat) +
-      geom_point(fill="red", shape=21, size = 5) +
-      labs(x = "Longitude", y = "Latitude", title = "Montgomery County, Maryland") +
+      aes(x = New.Lon, y = New.Lat, size = "Impact") +
+      geom_point(fill="red", shape=21) +
+      labs(x = "Longitude", y = "Latitude") +
       theme(legend.position = "none") +
-      coord_map()
+      coord_map() + ggtitle(paste("Montgomery County, MD - ", frame2[i,]$Time))
       
     print(night.map)
   }
